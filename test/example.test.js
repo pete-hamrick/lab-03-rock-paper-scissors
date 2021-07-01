dc3ac8322c826692045827b0a03aa2fcf822da51
Pete Hamrick <pete.hamrick@gmail.com>
2021-07-01 15:04:24 -0700

    wrote failing tests for wins, losses, and draws

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,55 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-
+import { didUserWin } from "../utils.js";
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('testing user win', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
-    
+        
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const win1 = didUserWin('rock', 'scissors');
+    const win2 = didUserWin('scissors', 'paper');
+    const win3 = didUserWin('paper', 'rock');
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(win1, true);
+    expect.equal(win2, true);
+    expect.equal(win3, true);
+});
+
+test('testing user loss', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const loss1 = didUserWin('rock', 'paper');
+    const loss2 = didUserWin('paper', 'scissors');
+    const loss3 = didUserWin('scissors', 'rock');
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(loss1, true);
+    expect.equal(loss2, true);
+    expect.equal(loss3, true);
+});
+
+test('testing a draw', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const draw1 = didUserWin('rock', 'paper');
+    const draw2 = didUserWin('paper', 'scissors');
+    const draw3 = didUserWin('scissors', 'rock');
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(draw1, true);
+    expect.equal(draw2, true);
+    expect.equal(draw3, true);
 });
